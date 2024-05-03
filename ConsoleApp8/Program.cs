595aa1953318f6b4185aa3a5c86e08d34173c16a
User <User@MAXDUST>
2024-05-03 20:26:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+delegate int FactorialDelegate(int n);
+
+delegate T SquareDelegate<T>(T x);
+
+class Program
+{
+    static int Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        return n * Factorial(n - 1);
+    }
+
+    static void Main()
+    {
+        FactorialDelegate factorialDelegate = Factorial;
+
+        int factorialOf3 = factorialDelegate(3);
+        Console.WriteLine( factorialOf3); 
+
+        SquareDelegate<int> squareDelegate = x => x * x;
+
+        int squareOf9 = squareDelegate(9);
+        Console.WriteLine( squareOf9); 
+    }
+}
